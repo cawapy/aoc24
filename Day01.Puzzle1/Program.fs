f90c44ec0000995b39fe2418dcbfcee7c2929659
cawapy <31598347+cawapy@users.noreply.github.com>
2024-12-01 17:45:55 +0100

    Adjust function and output names in Day01.Puzzle1

--- Day01.Puzzle1/Program.fs
+++ Day01.Puzzle1/Program.fs
@@ -1,7 +1,7 @@
 ﻿open System
 open TextInput
 
-let calculateDistanceSum (left: int list) (right: int list) =
+let totalDistanceSum (left: int list) (right: int list) =
     List.map2
         (fun l r -> abs (l - r))
         (left |> List.sort)
@@ -12,6 +12,6 @@ let input = Reader.readLines Console.In |> Parser.linesToInts
 let left = input |> List.map (List.item 0)
 let right = input |> List.map (List.item 1)
 
-let result = calculateDistanceSum left right
+let result = totalDistanceSum left right
 
-printfn $"Result: {result}"
+printfn $"Total distance sum: {result}"
