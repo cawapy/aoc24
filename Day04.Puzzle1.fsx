f4858ef8b0621f45c203bef8b24317fade8c6852
cawapy <31598347+cawapy@users.noreply.github.com>
2024-12-05 00:26:28 +0100

    Solve Day 4 Puzzle 1

---
+++ Day04.Puzzle1.fsx
@@ -0,0 +1,38 @@
+#load "TextInput/Reader.fsx"
+open System
+open TextInput
+
+let input = Reader.readLinesOrDefault Console.In [
+    "MMMSXXMASM"
+    "MSAMXMSMSA"
+    "AMXSXMAAMM"
+    "MSAMASMSMX"
+    "XMASAMXAMM"
+    "XXAMMXXAMA"
+    "SMSMSASXSS"
+    "SAXAMASAAA"
+    "MAMMMXMMMM"
+    "MXMXAXMASX"
+]
+
+let countWords (input: string list) (word: string) =
+    let isMatch (pos: int * int) (direction: int * int) =
+        let getChar (x: int) (y: int) =
+            if 0 <= y && y < input.Length && 0 <= x && x < input[y].Length then
+                input[y][x]
+            else
+                '_'
+        let x, y = pos
+        let dx, dy = direction
+        [0 .. word.Length - 1] |> List.forall (fun i -> word[i] = getChar (x + i * dx) (y + i * dy) )
+    let startPoints = [ 0 .. input.Length - 1 ]
+                    |> List.collect (fun y -> [ 0 .. input[y].Length - 1 ] |> List.map (fun x -> (x, y) ))
+    let directions = List.allPairs [ -1 .. 1 ] [ -1 .. 1 ]
+    List.allPairs startPoints directions |> List.where (fun pair -> isMatch (fst pair) (snd pair)) |> List.length
+
+let xmasCount (input: string list) =
+    countWords input "XMAS"
+
+let result = xmasCount input
+
+printfn $"XMAS appears {result} times"
