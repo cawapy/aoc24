8822e242c17098d524e5d9005a4eb5d0ffb33f33
cawapy <31598347+cawapy@users.noreply.github.com>
2024-12-05 01:00:04 +0100

    Do not use direction (0,0) as this might lead to wrong matches for words like "AAA"

--- Day04.Puzzle1.fsx
+++ Day04.Puzzle1.fsx
@@ -27,7 +27,7 @@ let countWords (input: string list) (word: string) =
         [0 .. word.Length - 1] |> List.forall (fun i -> word[i] = getChar (x + i * dx) (y + i * dy) )
     let startPoints = [ 0 .. input.Length - 1 ]
                     |> List.collect (fun y -> [ 0 .. input[y].Length - 1 ] |> List.map (fun x -> (x, y) ))
-    let directions = List.allPairs [ -1 .. 1 ] [ -1 .. 1 ]
+    let directions = List.allPairs [ -1 .. 1 ] [ -1 .. 1 ] |> List.except [(0,0)]
     List.allPairs startPoints directions |> List.where (fun pair -> isMatch (fst pair) (snd pair)) |> List.length
 
 let xmasCount (input: string list) =
