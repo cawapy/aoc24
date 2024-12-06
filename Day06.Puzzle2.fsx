17530ad87238544ebe8200d54943e5e014bf20ce
cawapy <31598347+cawapy@users.noreply.github.com>
2024-12-06 22:25:50 +0100

    Speed up by checking for obstacle caused loop by starting in front of obstacle rather initial start point

--- Day06.Puzzle2.fsx
+++ Day06.Puzzle2.fsx
@@ -18,68 +18,66 @@ type Rule = { Cursor: char; direction: int*int; CursorAfterCollision: char }
 
 let findObstacles (stringListMap: string list) =
 
-    // array preferred over list for frequent random access
-    let run (map: char array array) =
+    let rules = [ { Cursor = '^'; direction = ( 0, -1); CursorAfterCollision = '>' };
+                  { Cursor = '>'; direction = ( 1,  0); CursorAfterCollision = 'v' };
+                  { Cursor = 'v'; direction = ( 0,  1); CursorAfterCollision = '<' };
+                  { Cursor = '<'; direction = (-1,  0); CursorAfterCollision = '^' } ]
 
-        let rules = [ { Cursor = '^'; direction = ( 0, -1); CursorAfterCollision = '>' };
-                      { Cursor = '>'; direction = ( 1,  0); CursorAfterCollision = 'v' };
-                      { Cursor = 'v'; direction = ( 0,  1); CursorAfterCollision = '<' };
-                      { Cursor = '<'; direction = (-1,  0); CursorAfterCollision = '^' } ]
+    let startPosition (map: char array array) =
+        let isCursor (pos: int * int) =
+            let x, y = pos
+            match map[y][x] with | '>' | 'v' | '<' | '^' -> true | _ -> false
+        [0 .. map.Length-1]
+                        |> Seq.collect (fun y -> [0 .. map[y].Length - 1] |> Seq.map (fun x -> (x, y)))
+                        |> Seq.where isCursor
+                        |> Seq.item 0
 
-        let fieldValue (x: int) (y: int) =
-            if 0 <= y && y < map.Length && 0 <= x && x < map[y].Length then
-                map[y][x]
-            else ' '
+    let fieldValue (map: char array array) (x: int) (y: int) =
+        if 0 <= y && y < map.Length && 0 <= x && x < map[y].Length then
+            map[y][x]
+        else ' '
 
-        let startPosition =
-            let isCursor (pos: int * int) =
-                let x, y = pos
-                match map[y][x] with | '>' | 'v' | '<' | '^' -> true | _ -> false
-            [0 .. map.Length-1]
-                            |> Seq.collect (fun y -> [0 .. map[y].Length - 1] |> Seq.map (fun x -> (x, y)))
-                            |> Seq.where isCursor
-                            |> Seq.item 0
+    let rec step (map: char array array) (guardX: int) (guardY: int) (guard: char) (steps: (int * int * char) list) =
 
-        let rec step (guardX: int) (guardY: int) (guard: char) (steps: (int * int * char) list) =
+        if List.contains (guardX, guardY, guard) steps then // already been here, going in same direction -> loop
+            []
+        else
+            let collisionRule = rules |> Seq.where (fun r -> r.Cursor = guard) |> Seq.item 0
+            let nextX, nextY = (guardX + fst collisionRule.direction, guardY + snd collisionRule.direction)
+            let nextFieldValue = fieldValue map nextX nextY
 
-            if List.contains (guardX, guardY, guard) steps then // already been here, going in same direction -> loop
-                []
-            else
-                let collisionRule = rules |> Seq.where (fun r -> r.Cursor = guard) |> Seq.item 0
-                let nextX, nextY = (guardX + fst collisionRule.direction, guardY + snd collisionRule.direction)
-                let nextFieldValue = fieldValue nextX nextY
+            match nextFieldValue with
 
-                match nextFieldValue with
+            // obstacle ahead: turn and continue without doing a true step
+            | '#' -> step map guardX guardY collisionRule.CursorAfterCollision steps
 
-                // obstacle ahead: turn and continue without doing a true step
-                | '#' -> step guardX guardY collisionRule.CursorAfterCollision steps
+            // free field (or guard's starting position) ahead: record step and continue: ...
+            | '.' | '^' | '>' | 'v' | '<'
+                  -> step map nextX nextY guard ((guardX,guardY,guard) :: steps)
 
-                // free field (or guard's starting position) ahead: record step and continue: ...
-                | '.' | '^' | '>' | 'v' | '<'
-                      -> step nextX nextY guard ((guardX,guardY,guard) :: steps)
-
-                // edge of map ahead: end journey with last step
-                | ' ' -> ((guardX,guardY,guard) :: steps)
-                | _ -> failwith $"Unexpected value of next field: '{nextFieldValue}"
-
-        let x, y = startPosition
-        step x y map[y].[x] []
+            // edge of map ahead: end journey with last step
+            | ' ' -> ((guardX,guardY,guard) :: steps)
+            | _ -> failwith $"Unexpected value of next field: '{nextFieldValue}"
 
     let makeArray (input: string list) =
         input |> List.map _.ToCharArray() |> List.toArray
 
-    let path = run (makeArray stringListMap) |> List.map (fun (x, y, _) -> (x, y)) |> List.distinct
+    let originalMap = (makeArray stringListMap)
+    let x, y = startPosition originalMap
+    let d = fieldValue originalMap x y
+    let path = step originalMap x y d [] |> List.rev |> List.distinctBy (fun (x, y, _) -> (x, y))
 
     // try to place obstacles along path and check again for loop
-    let obstacleCausesLoop (position: int * int) =
-        let x, y = position
-        if stringListMap[y][x] = '.' then
+    let obstacleCausesLoop (startPosition: int * int * char) (obstaclePosition: int * int * _) =
+        let x0, y0, d = startPosition
+        let xX, yX, _ = obstaclePosition
+        if stringListMap[yX][xX] = '.' then
             let modifiedMap = makeArray stringListMap
-            modifiedMap[y][x] <- '#'
-            run modifiedMap = []
+            modifiedMap[yX][xX] <- '#'
+            step modifiedMap x0 y0 d [] = []
         else
             false
-    let positions = path |> List.where obstacleCausesLoop
+    let positions = List.pairwise path |> List.where (fun pair -> obstacleCausesLoop (fst pair) (snd pair))
     positions |> List.length
 
 let numberOfObstacles = findObstacles input
