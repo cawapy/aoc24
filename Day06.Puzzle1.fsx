ac205867cc72e39c4757b0eb2ca9f978cc4c6d2c
cawapy <31598347+cawapy@users.noreply.github.com>
2024-12-06 18:25:07 +0100

    Restructure guardSteps function - use same map in all sub functions without passing

--- Day06.Puzzle1.fsx
+++ Day06.Puzzle1.fsx
@@ -16,11 +16,21 @@ let input = Reader.readInput { Reader.readInputOptions with
 
 type Rule = { Cursor: char; direction: int*int; CursorAfterCollision: char }
 
-let guardSteps (map: string list) =
-    let rules = [ { Cursor = '^'; direction = (0,-1); CursorAfterCollision = '>' };
-                   { Cursor = '>'; direction = (1,0); CursorAfterCollision = 'v' };
-                   { Cursor = 'v'; direction = (0,1); CursorAfterCollision = '<' };
-                   { Cursor = '<'; direction = (-1,0); CursorAfterCollision = '^' } ]
+let guardSteps (stringListMap: string list) =
+
+    // array preferred over list for frequent random access
+    let map = stringListMap |> List.map _.ToCharArray() |> List.toArray
+
+    let rules = [ { Cursor = '^'; direction = ( 0, -1); CursorAfterCollision = '>' };
+                  { Cursor = '>'; direction = ( 1,  0); CursorAfterCollision = 'v' };
+                  { Cursor = 'v'; direction = ( 0,  1); CursorAfterCollision = '<' };
+                  { Cursor = '<'; direction = (-1,  0); CursorAfterCollision = '^' } ]
+
+    let fieldValue (x: int) (y: int) =
+        if 0 <= y && y < map.Length && 0 <= x && x < map[y].Length then
+            map[y][x]
+        else ' '
+
     let startPosition =
         let isCursor (pos: int * int) =
             let x, y = pos
@@ -29,27 +39,28 @@ let guardSteps (map: string list) =
                         |> Seq.collect (fun y -> [0 .. map[y].Length - 1] |> Seq.map (fun x -> (x, y)))
                         |> Seq.where isCursor
                         |> Seq.item 0
-    let rec step (map: char array array) (guardX: int) (guardY: int) (guard: char) (steps: (int * int) list) =
-        let fieldValue (x: int) (y: int) =
-            if 0 <= y && y < map.Length && 0 <= x && x < map[y].Length then
-                map[y][x]
-            else ' '
+
+    let rec step (guardX: int) (guardY: int) (guard: char) (steps: (int * int) list) =
+
         let collisionRule = rules |> Seq.where (fun r -> r.Cursor = guard) |> Seq.item 0
         let nextX, nextY = (guardX + fst collisionRule.direction, guardY + snd collisionRule.direction)
         let nextFieldValue = fieldValue nextX nextY
+
         match nextFieldValue with
+
         // obstacle ahead: turn and continue without doing a true step
-        | '#' -> step map guardX guardY collisionRule.CursorAfterCollision steps
+        | '#' -> step guardX guardY collisionRule.CursorAfterCollision steps
 
         // free field (or guard's starting position) ahead: record step and continue: ...
         | '.' | '^' | '>' | 'v' | '<'
-              -> step map nextX nextY guard ((guardX,guardY) :: steps)
+              -> step nextX nextY guard ((guardX,guardY) :: steps)
+
         // edge of map ahead: end journey with last step
         | ' ' -> ((guardX,guardY) :: steps)
         | _ -> failwith $"Unexpected value of next field: '{nextFieldValue}"
+
     let x, y = startPosition
-    let mapArray = input |> List.map _.ToCharArray() |> List.toArray
-    let allSteps = step mapArray x y mapArray[y].[x] []
+    let allSteps = step x y map[y].[x] []
     allSteps |> List.distinct |> List.length
 
 let steps = guardSteps input
