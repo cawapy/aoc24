c1cbd5f221fd115aac63dc2369a3e3a9e3d5f027
cawapy <31598347+cawapy@users.noreply.github.com>
2024-12-17 20:51:50 +0100

    Calculate cost during tracking paths, aborting longer options

--- Day16.Puzzle1.fsx
+++ Day16.Puzzle1.fsx
@@ -39,40 +39,53 @@ let sample2 = [
     "#################";
 ]
 
-let lines = Reader.readInput { Reader.readInputOptions with Default = sample1 }
+let lines = Reader.readInput { Reader.readInputOptions with Default = sample2 }
 let maze = lines |> List.map _.ToCharArray() |> List.toArray
 
 type Maze = char array array
 type Pos = int * int
 type Path = Pos list
 
-let findPaths (maze: Maze) =
-    let rec _findPaths (x: int, y: int) (dx: int, dy: int) (path: Path) =
-        match maze[y][x] with
-        | '.' | 'S' ->
-            let pStraight: Path list = if List.contains (x+dx, y+dy) path then [] else _findPaths (x+dx, y+dy) (dx, dy) ((x, y) :: path)
-            let pLeft: Path list = if List.contains (x+dy, y-dx) path then [] else _findPaths (x+dy, y-dx) (dy, -dx) ((x, y) :: path)
-            let pRight: Path list = if List.contains (x-dy, y+dx) path then [] else _findPaths (x-dy, y+dx) (-dy, dx) ((x, y) :: path)
-            pStraight @ pLeft @ pRight
-        | 'E' -> printf "P"; [path |> List.rev]
-        | _ -> []
+let findPath (maze: Maze) =
+    let getCost (input: ('a * int) option) =
+        match input with
+        | Some (_, cost) -> Some cost
+        | _ -> None
+    let minCost (left: int option) (right: int option) =
+        match (left, right) with
+        | None, None -> None
+        | Some _, None -> left
+        | None, Some _ -> right
+        | Some l, Some r when l <= r -> left
+        | _ -> right
+    let selectMin (left: ('a * int) option) (right: ('a * int) option) =
+        match (left, right) with
+        | None, None -> None
+        | Some _, None -> left
+        | None, Some _ -> right
+        | Some (_, l), Some (_, r) when l < r -> left
+        | _ -> right
+    let rec _findPaths (x: int, y: int) (dx: int, dy: int) (path: Path) (cost: int) (maxCost: int option) =
+        match maxCost with
+        | Some n when n < cost -> printfn $"cancel: {cost} > max ({n})"; None
+        | _ ->
+            match maze[y][x] with
+            | '.' | 'S' ->
+                let straightOption = if List.contains (x+dx, y+dy) path then None else _findPaths (x+dx, y+dy) (dx, dy) ((x, y) :: path) (cost+1) maxCost
+
+                let leftOption = if List.contains (x+dy, y-dx) path then None else _findPaths (x+dy, y-dx) (dy, -dx) ((x, y) :: path) (cost+1001) (getCost straightOption)
+
+                let nonRightOption = selectMin straightOption leftOption
+                let rightOption = if List.contains (x-dy, y+dx) path then None else _findPaths (x-dy, y+dx) (-dy, dx) ((x, y) :: path) (cost+1001) (getCost nonRightOption)
+                selectMin nonRightOption rightOption
+            | 'E' ->
+                let result: Path = path |> List.rev
+                Some (result, cost)
+            | _ -> None
     let start = [0..maze.Length-1] |> List.collect (fun y -> [0..maze[y].Length-1] |> List.map (fun x -> (x, y))) |>
                 List.where (fun (x, y) -> maze[y][x] = 'S') |> List.item 0
-    _findPaths start (1, 0) []
+    _findPaths start (1, 0) [] 0 None
 
-let pathScore (path: Path) =
-    let getDirection ((x0, y0): Pos, (x1, y1): Pos) =
-        match (x1-x0, y1-y0) with
-        | -1,  0 -> 'w'
-        | +1,  0 -> 'e'
-        |  0, -1 -> 'n'
-        |  0, +1 -> 's'
-        | _ -> failwith "Unexpected step"
-    let directions: char list = path |> List.pairwise |> List.map getDirection |> List.append ['e']
-    let turns = directions |> List.pairwise |> List.map (fun (a, b) -> if a <> b then 1 else 0) |> List.sum
-    turns * 1000 + path.Length
-
-let paths = findPaths maze
-let minScore = paths |> List.map pathScore |> List.min
-
-printfn $"Lowest score is {minScore}"
+match findPath maze with
+| Some (_, score) -> printfn $"Lowest score is {score}"
+| _ -> printfn "Could not find path"
