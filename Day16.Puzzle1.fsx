3dc6209e3088c8bbd9f9d66929e7f1702a27df78
cawapy <31598347+cawapy@users.noreply.github.com>
2024-12-17 23:17:35 +0100

    Simplify maze solver

--- Day16.Puzzle1.fsx
+++ Day16.Puzzle1.fsx
@@ -47,10 +47,6 @@ type Pos = int * int
 type Path = Pos list
 
 let findPath (maze: Maze) =
-    let getCost (input: ('a * int) option) =
-        match input with
-        | Some (_, cost) -> Some cost
-        | _ -> None
     let selectMin (left: ('a * int) option) (right: ('a * int) option) =
         match (left, right) with
         | None, None -> None
@@ -59,29 +55,23 @@ let findPath (maze: Maze) =
         | Some (_, l), Some (_, r) when l < r -> left
         | _ -> right
     let costArray = maze |> Array.map (fun row -> Array.replicate row.Length 0x7fffffff)
-    let rec _findPaths (x: int, y: int) (dx: int, dy: int) (path: Path) (cost: int) (maxCost: int option) =
+    let rec _findPaths (x: int, y: int) (dx: int, dy: int) (path: Path) (cost: int) =
         if costArray[y][x] < cost then None
         else
             costArray[y][x] <- cost
-            match maxCost with
-            | Some n when n < cost -> printfn $"cancel: {cost} > max ({n})"; None
-            | _ ->
-                match maze[y][x] with
-                | '.' | 'S' ->
-                    let straightOption = if List.contains (x+dx, y+dy) path then None else _findPaths (x+dx, y+dy) (dx, dy) ((x, y) :: path) (cost+1) maxCost
-
-                    let leftOption = if List.contains (x+dy, y-dx) path then None else _findPaths (x+dy, y-dx) (dy, -dx) ((x, y) :: path) (cost+1001) (getCost straightOption)
-
-                    let nonRightOption = selectMin straightOption leftOption
-                    let rightOption = if List.contains (x-dy, y+dx) path then None else _findPaths (x-dy, y+dx) (-dy, dx) ((x, y) :: path) (cost+1001) (getCost nonRightOption)
-                    selectMin nonRightOption rightOption
-                | 'E' ->
-                    let result: Path = path |> List.rev
-                    Some (result, cost)
-                | _ -> None
+            match maze[y][x] with
+            | '.' | 'S' ->
+                let straightOption = if List.contains (x+dx, y+dy) path then None else _findPaths (x+dx, y+dy) (dx, dy) ((x, y) :: path) (cost+1)
+                let leftOption = if List.contains (x+dy, y-dx) path then None else _findPaths (x+dy, y-dx) (dy, -dx) ((x, y) :: path) (cost+1001)
+                let rightOption = if List.contains (x-dy, y+dx) path then None else _findPaths (x-dy, y+dx) (-dy, dx) ((x, y) :: path) (cost+1001)
+                selectMin (selectMin straightOption leftOption) rightOption
+            | 'E' ->
+                let result: Path = path |> List.rev
+                Some (result, cost)
+            | _ -> None
     let start = [0..maze.Length-1] |> List.collect (fun y -> [0..maze[y].Length-1] |> List.map (fun x -> (x, y))) |>
                 List.where (fun (x, y) -> maze[y][x] = 'S') |> List.item 0
-    _findPaths start (1, 0) [] 0 None
+    _findPaths start (1, 0) [] 0
 
 let sw = System.Diagnostics.Stopwatch.StartNew()
 match findPath maze with
