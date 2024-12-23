fec2504c75a0acc450ed8e6b3f5da1bd18bbc272
cawapy <31598347+cawapy@users.noreply.github.com>
2024-12-23 16:02:57 +0100

    More concise code in Day22

--- Day22.fsx
+++ Day22.fsx
@@ -8,14 +8,12 @@ let parseSecret : string -> secretT =
     System.Int64.Parse
 
 let nextSecret (secret: secretT) : secretT =
-    let mix value (secret: secretT) : secretT =
-        value ^^^ secret
 
-    let prune (secret: secretT) : secretT =
-        secret % secretT 16777216
+    let mix sec value = sec ^^^ value
+    let prune sec = sec % (secretT 16777216)
 
-    let step1 (s: secretT) : secretT = (s * (secretT   64)) |> mix s |> prune
-    let step2 (s: secretT) : secretT = (s / (secretT   32)) |> mix s |> prune
-    let step3 (s: secretT) : secretT = (s * (secretT 2048)) |> mix s |> prune
+    let step1 sec = (sec * (secretT   64)) |> mix sec |> prune
+    let step2 sec = (sec / (secretT   32)) |> mix sec |> prune
+    let step3 sec = (sec * (secretT 2048)) |> mix sec |> prune
 
     secret |> step1 |> step2 |> step3
